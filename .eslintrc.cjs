7201367a8ab92a420afa5a7362bf8ed656a8f8fc
Catarina Coelho <k.tarina56@gmail.com>
2025-05-21 14:34:18 -0300

    eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+const { default: ts } = require("typescript");
+
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: ["./tsconfig.json", "tsconfig.node.json"],
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ["react", "react-hooks", "@typescript-eslint", "tailwindcss"],
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:@typescript-eslint/stylistic-type-checked",
+		"plugin:tailwindcss/recommended",
+	],
+	rules: {
+		"react/react-in-jsx-scope": "off",
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+		"@typescript-eslint/no-unused-vars": "warn",
+		"@typescript-eslint/no-unsafe-call": "off",
+		"@typescript-eslint/no-unsafe-assignment": "off",
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+};
